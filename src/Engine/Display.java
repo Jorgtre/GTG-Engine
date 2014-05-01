c5c2b4557950f1cec8d237bd8b13e4ecdd541d61
jorgtre <jorgen_smerud@hotmail.com>
2014-05-01 21:39:45 +0200

    tt1

--- src/Engine/Display.java
+++ src/Engine/Display.java
@@ -4,10 +4,14 @@ package Engine;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.DisplayMode;
 
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+
 public class Display {
 
     private int width = 0;
     private int height = 0;
+    private int FPSCap = 0;
 
     public Display(int w, int h)
     {
@@ -31,6 +35,26 @@ public class Display {
     {
         this.width = newWidth;
         this.height = newHeight;
+        glViewport( 0, 0, this.width, this.height );
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, this.width, this.height , 0, 1, -1);
+        glMatrixMode(GL_MODELVIEW);
+        glEnable(GL_TEXTURE_2D);
+    }
+    public boolean isCloseRequested()
+    {
+        return org.lwjgl.opengl.Display.isCloseRequested();
+    }
+    public void update()
+    {
+        org.lwjgl.opengl.Display.update();
+        if (FPSCap > 0)
+        {
+            org.lwjgl.opengl.Display.sync(FPSCap);
+        }
     }
 
 
--- src/Engine/Game.java
+++ src/Engine/Game.java
@@ -1,8 +1,27 @@
 package Engine;
 
+import Engine.Display;
 
 public class Game {
 
+    private Display window = null;
+
+    public Game()
+    {
+
+    }
+    public void construct()
+    {
+        window = new Display(800,600);
+    }
+    public void destroy()
+    {
+
+    }
+    public void run()
+    {
+
+    }
 
 
 
